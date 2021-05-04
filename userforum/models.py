193adc1e5353b08823067edd53f5461264d62803
AntKarkun <danuulka.kar@gmail.com>
2021-05-05 01:10:24 +0300

    fix views count null value

--- userforum/models.py
+++ userforum/models.py
@@ -6,7 +6,7 @@ class Articles(models.Model):
     text = models.CharField(max_length=1000)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     publish_time = models.DateTimeField(auto_now=False)
-    views_count = models.PositiveIntegerField()
+    views_count = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return f'{self.title} by {self.autor}'
